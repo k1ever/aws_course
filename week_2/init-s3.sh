feb93fb859bd0ebd3836ce050ec1de9966018a64
Serhii Voitenko <voitenko.sv@gmail.com>
2022-10-24 21:05:50 +0300

    init-s3.sh script fixed and uncommented

--- week_2/init-s3.sh
+++ week_2/init-s3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#echo "AWS course is awesome!" > test_file.txt
+echo "AWS course is awesome!" > test_file.txt
 BUCKET_NAME="svoitenko-bucket-1"
-#aws s3api create-bucket --bucket $BUCKET_NAME --create-bucket-configuration LocationConstraint=us-west-2
-##aws s3api create-bucket --bucket $BUCKET_NAME --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
-#aws s3api put-bucket-versioning --bucket $BUCKET_NAME --versioning-configuration MFADelete=Disabled,Status=Enabled
+aws s3api create-bucket --bucket $BUCKET_NAME --create-bucket-configuration LocationConstraint=us-west-2
+aws s3api create-bucket --bucket $BUCKET_NAME --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+aws s3api put-bucket-versioning --bucket $BUCKET_NAME --versioning-configuration MFADelete=Disabled,Status=Enabled
 aws s3 cp test_file.txt s3://$BUCKET_NAME/
 aws s3 ls s3://$BUCKET_NAME/
