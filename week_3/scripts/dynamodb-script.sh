99ae4be5ff0a07ec4d4697f40b2b6e3c5ceb6db0
Serhii Voitenko <voitenko.sv@gmail.com>
2022-11-03 22:26:48 +0200

    dynamodb-script.sh script logging added

--- week_3/scripts/dynamodb-script.sh
+++ week_3/scripts/dynamodb-script.sh
@@ -7,9 +7,11 @@ TABLE_NAME="aws_students"
 aws dynamodb list-tables --region us-west-2
 
 # Put item into the table
+echo "INSERTING DATA TO DYNAMODB TABLE $TABLE_NAME"
 aws dynamodb put-item --table-name $TABLE_NAME --item '{"id": {"N":"1"}, "name":{"S":"Sergii"}}' --region us-west-2
 aws dynamodb put-item --table-name $TABLE_NAME --item '{"id": {"N":"2"}, "name":{"S":"Anna"}}' --region us-west-2
 aws dynamodb put-item --table-name $TABLE_NAME --item '{"id": {"N":"3"}, "name":{"S":"Mitya"}}' --region us-west-2
+echo "DATA INSERTION COMPLETED"
 
 # Get items from the table
 aws dynamodb get-item --table-name $TABLE_NAME --key '{"id": {"N":"3"}}' --region us-west-2
